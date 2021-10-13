bf9f9440e88903a8a25408ac5441ce47317a1408
NickSchoenmakers <71122447+NickSchoenmakers@users.noreply.github.com>
2021-10-13 11:26:42 +0200

    date fixed
    het laat nu de huidige datum zien en update hem continu

--- index.php
+++ index.php
@@ -27,10 +27,22 @@
 <?php require_once 'header.php'; ?>
 
 <!-- you can see the time on the page -->
-<?php
-$date = date("Y-m-d H:i:s");
-echo $date;
-?>
+<script type="text/javascript"> 
+function display_c(){
+var refresh=1000; // Refresh rate in milli seconds
+mytime=setTimeout('display_ct()',refresh)
+}
+
+function display_ct() {
+  var x = new Date()
+var x1=x.getMonth() + 1+ "/" + x.getDate() + "/" + x.getFullYear(); 
+x1 = x1 + " - " +  x.getHours( )+ ":" +  x.getMinutes() + ":" +  x.getSeconds();
+document.getElementById('ct').innerHTML = x1;
+display_c();
+ }
+</script>
+<body onload=display_ct();>
+<span id='ct' ></span>
 
   <!-- Add your site or application content here -->
 
