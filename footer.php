92c89b42089fa116c67c1ef2598abec50aec4b7f
jenniferwillems <70897990+jenniferwillems@users.noreply.github.com>
2021-10-18 09:28:42 +0200

    footer left 0

--- footer.php
+++ footer.php
@@ -27,13 +27,14 @@
 </head>
 
 <footer>
-    <div class="footers">
+  <div>
         <div class="footer-links">
             <a href="https://twitter.com/home?lang=nl"><img src="<?php echo $baseurl; ?>img/twitter.png" alt="twitter"></a>  
             <a href="https://www.facebook.com/Startpagina.nl/"><img src="<?php echo $baseurl; ?>img/facebook.jpg" alt="facebook"></a>  
             <a href="https://www.youtube.com/"><img src="<?php echo $baseurl; ?>img/youtube.jpg" alt="youtube"></a>  
             <a href="https://www.instagram.com/"><img src="<?php echo $baseurl; ?>img/instagram.png" alt="instagram"></a> 
         </div> 
-        <p>&copy; 2021 CampCode</p>    
-    </div>      
+        
+        <p>&copy; 2021 CampCode</p>   
+  </div>      
 </footer>
