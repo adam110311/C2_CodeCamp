a2115ae8751b7a70ad5bbb3e60564b6a62d2e639
Mylo <36931363+gitmylo@users.noreply.github.com>
2021-11-08 09:45:19 +0100

    fix actually actually actually actuallyactuallyactually actually

--- backend/config.php
+++ backend/config.php
@@ -1,7 +1,7 @@
 <?php
 if(is_dir($_SERVER['DOCUMENT_ROOT'] . '/c2_codecamp')){
-  $base_url = "http://localhost/c2_codecamp/";
+  $baseurl = "http://localhost/c2_codecamp/";
 }
 else{
-  $base_url = "https://c2_codecamp.test/";
+  $baseurl = "https://c2_codecamp.test/";
 }
